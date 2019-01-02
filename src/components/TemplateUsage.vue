d65401191a861b0403f04ee6514bcafaf1503e0e
justdoit3651212 <justdoit3651212@gmail.com>
2019-01-02 18:05:44 +0800

    add netParticle

---
+++ src/components/TemplateUsage.vue
@@ -0,0 +1,44 @@
+<!--
+ * @author userjustdoit
+ * @des  模板基本用法
+ -->
+<template>
+    <div class="container">
+        <canvas-template ref='canvasRef' @draw="draw"></canvas-template>
+    </div>
+</template>
+
+<script>
+    import CanvasTemplate from '@/components/CanvasTemplate';
+    import Constans from '@/base/constans.js';
+    export default {
+        name: "<your fileName>",
+        components: {CanvasTemplate},
+        data() {
+            return {
+                hPctAll:0,//h方向的总pct
+            };
+        },
+        methods: {
+            draw(ctx){
+                this.hPctAll=this.getHPct()
+            },
+            clearAll(ctx){
+                ctx.save();
+                ctx.scale(Constans.definition,Constans.definition);
+                ctx.clearRect(0, 0, this.toPx(750), this.toPx(this.hPctAll));
+                ctx.restore();
+            },
+            getCanvasRef(){
+                return this.$refs.canvasRef;
+            }
+        },
+        computed: {},
+    }
+</script>
+
+<style scoped>
+    .container{
+        margin-top: 60px;
+    }
+</style>
